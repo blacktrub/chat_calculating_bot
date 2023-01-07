e6e5a5cc51a12fb28d7d0f1bca496e8da47a11a0
blacktrub <lightlocdog@gmail.com>
2023-01-07 17:48:11 +0300

    work with complex expressions

--- bot/main.py
+++ bot/main.py
@@ -1,5 +1,7 @@
 import decimal
+from decimal import Decimal
 import operator
+from typing import List
 
 import telebot
 
@@ -8,36 +10,69 @@ from constants import TOKEN
 bot = telebot.TeleBot(TOKEN)
 
 OPERATION_MAP = {
-    "плюс": operator.add,
     "+": operator.add,
-    "минус": operator.sub,
     "-": operator.sub,
-    "разделить": operator.truediv,
     "/": operator.truediv,
-    "умножить": operator.mul,
     "*": operator.mul,
 }
 
 
+def is_expression(s: str) -> bool:
+    s = s.strip().replace(" ", "")
+    for x in (" ", *"".join(str(x) for x in range(10)), *OPERATION_MAP.keys()):
+        s = s.replace(x, "")
+    return s == ""
+
+
+def expression_parser(s: str) -> List[str]:
+    s = s.strip().replace(" ", "")
+    res = []
+    j = i = 0
+    while i < len(s):
+        if s[i] in OPERATION_MAP.keys():
+            res.append(s[j:i])
+            res.append(s[i])
+            j = i + 1
+        i += 1
+
+    res.append(s[j:i].strip())
+    return res
+
+
+def calculate(operations: List[str]) -> Decimal:
+    # Frist of all we perform high priority operators
+    i = 0
+    while i < len(operations):
+        if operations[i] in ("*", "/"):
+            first, second = Decimal(operations[i - 1]), Decimal(operations[i + 1])
+            operations[i - 1] = OPERATION_MAP[operations[i]](first, second)
+            operations.pop(i)
+            operations.pop(i)
+        else:
+            i += 1
+
+    # Now we can do the rest
+    i = 0
+    while i < len(operations):
+        if not operations[i].isnumeric():
+            first, second = Decimal(operations[i - 1]), Decimal(operations[i + 1])
+            operations[i - 1] = OPERATION_MAP[operations[i]](first, second)
+            operations.pop(i)
+            operations.pop(i)
+        else:
+            i += 1
+
+    return Decimal(operations[0])
+
+
 @bot.message_handler()
 def listen_all_messages(message):
-    # bot can work with messages from 3 part
-    if len(str(message.text).split()) != 3:
+    if not is_expression(message.text):
         return
 
-    one, two, three = (x.strip() for x in str(message.text).split())
-    if two not in OPERATION_MAP:
-        return
-
+    operations = expression_parser(message.text)
     try:
-        one, three = float(one), float(three)
-    except ValueError:
-        return
-
-    try:
-        result = decimal.Decimal(OPERATION_MAP[two](one, three)).quantize(
-            decimal.Decimal(".01"), decimal.ROUND_05UP
-        )
+        result = calculate(operations)
     except (ZeroDivisionError, decimal.InvalidOperation):
         return
 
